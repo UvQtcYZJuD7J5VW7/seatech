a484ce409c40b77eebfc492a46841098211d60ad
UvQtcYZJuD7J5VW7 <90563484+UvQtcYZJuD7J5VW7@users.noreply.github.com>
2022-03-03 10:28:48 +0100

    Update and rename trajet.cpp to DELEPIERE_TP.cpp

--- POO/C++/TD7 (noté)/DELEPIERE_TP.cpp
+++ POO/C++/TD7 (noté)/DELEPIERE_TP.cpp
@@ -5,59 +5,81 @@ using namespace std;
 class Trajet{ // Définition de la classe Trajet
     protected:
         // Attributs
-        float distance; // Attribut distance en réel
+        float distance; // Attribut distance (réel) en km
 
     public:
-        // Définition des méthodes
-        // Getter et Setter
+        // Méthodes
+        // Get/Set
         float getDistance() const; // Renvoie la valeur de l'attribut distance
-        void setDistance(float);
+        void setDistance(float); // Définit la valeur de l'attribut distance
 
-        Trajet();
-        Trajet(float);
-        Trajet(const Trajet&);
-        virtual ~Trajet();
+        // Constructeur et destructeur
+        Trajet(); // Constructeur par défaut
+        Trajet(float); // Constructeur paramétré
+        Trajet(const Trajet&); // Constructeur par recopie
+        virtual ~Trajet(); // Destructeur
 
-        friend ostream& operator<<(ostream&, Trajet&);
+        // Surcharge d'opérateurs
+        Voyage& operator=(const Voyage&); // Surcharge de l'opérateur égal
+        friend ostream& operator<<(ostream&, Trajet&); // Surcharge de l'opérateur affichage
 };
 
+// Get/Set distance
 float Trajet::getDistance() const{
-    return distance;
+    return distance; // Renvoie la valeur de l'attribut distance
 }
 
 void Trajet::setDistance(float d){
-    distance = abs(d);
+    distance = abs(d); // Définit la valeur de l'attribut distance par valeur absolue
 }
 
+// Constructeur par défaut
 Trajet::Trajet(){
     setDistance(0);
     cout << "Construction d'un Trajet par défaut" << endl;
 }
 
+// Constructeur paramétré
 Trajet::Trajet(float i){
     setDistance(i);
     cout << "Construction d'un Trajet par paramètre" << endl;
 }
 
+// Constructeur par recopie
 Trajet::Trajet(const Trajet& t){
     setDistance(t.getDistance());
     cout << "Construction d'un Trajet par recopie" << endl;
 }
 
+// Destructeur
 Trajet::~Trajet(){
     cout << "Destruction d'un Trajet" << endl;
 }
 
+// Surcharge de l'opérateur affichage
 ostream& operator<<(ostream& stream, Trajet& trajet){
-    stream << trajet.getDistance() << " km" << endl;
-    return stream;
+    stream << trajet.getDistance() << " km" << endl; // Mise en forme du canal de sortie
+    return stream; // Renvoie le canal de sortie
 }
 
-class Voyage : public Trajet{
+// Surcharge de l'opérateur égal
+Trajet& Trajet::operator=(const Trajet& trajet){
+    // Vérification de l'auto-affectation
+    if(this == &trajet)
+	{
+		cout << "Auto-affectation repérée" << endl;
+		return *this;
+	}
+    setDistance(trajet.getDistance()); // Recopie de l'attribut distance
+    return *this;
+}
+
+class Voyage : public Trajet{ // Définition de la classe Voyage, héritière de Trajet
     protected:
-        float vitesse;
+        float vitesse; // Définition d'un nouvel attribut vitesse
 
     public:
+        // Définition de nouvelles méthodes (constructeurs, get/set)
         float getVitesse() const;
         void setVitesse(float);
         float getDuree();
@@ -68,7 +90,6 @@ class Voyage : public Trajet{
         virtual ~Voyage();
 
         Voyage& operator=(const Voyage&);
-
         friend ostream& operator<<(ostream&, Voyage&);
 };
 
@@ -105,7 +126,7 @@ Voyage::~Voyage(){
 }
 
 ostream& operator<<(ostream& stream, Voyage& voyage){
-    stream << voyage.getDistance() << " km à " << voyage.getVitesse() << " km/h (Duréee " << voyage.getDuree() << "h)" << endl;
+    stream << voyage.getDistance() << " km à " << voyage.getVitesse() << " km/h (Durée " << voyage.getDuree() << "h)" << endl;
     return stream;
 }
 
